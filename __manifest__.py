e6a68ad3a9a774ac3392a7d637a53df56b3b9a0c
gabycardenas1 <gaby.01221@gmail.com>
2025-10-02 10:07:23 -0500

    Segundo commit

--- __manifest__.py
+++ __manifest__.py
@@ -22,13 +22,15 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml'
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+#    'demo': [
+ #       'demo/demo.xml',
+#    ],
+    'aplication': True,
+    'license': 'AGPL-3'
 }
 
--- models/models.py
+++ models/models.py
@@ -2,18 +2,17 @@
 
 # from odoo import models, fields, api
 
+class biblioteca(models.Model):
+     _name = 'biblioteca.libro'
+     _description = 'biblioteca.biblioteca'
 
-# class biblioteca(models.Model):
-#     _name = 'biblioteca.biblioteca'
-#     _description = 'biblioteca.biblioteca'
+     name = fields.Char()
+     value = fields.Integer()
+     value2 = fields.Float(compute="_value_pc", store=True)
+     description = fields.Text()
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+     @api.depends('value')
+     def _value_pc(self):
+         for record in self:
+             record.value2 = float(record.value) / 100
 
